178476e808c19b38d0d92cd186a508b39b2212d3
github username <github email address>
2019-08-27 01:32:31 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -3,4 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+// const h = document.querySelector(".App-header")
+// h.innerHTML = "now"
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
